bd5ffb8be6cf77ac99d84e1655153ee3b8fa8ffe
Samuel Childers <samuel14920@gmail.com>
2020-10-01 16:18:41 -0400

    button test

--- app/src/main/java/com/android/gang/anabolix/MainActivity.java
+++ app/src/main/java/com/android/gang/anabolix/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mKevinButton;
+    private Button mSamButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Hello, this is Kevin!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mSamButton = findViewById(R.id.sam_button);
+        mSamButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hi, I'm Sam!", Toast.LENGTH_SHORT).show();
             }
         });
     }
