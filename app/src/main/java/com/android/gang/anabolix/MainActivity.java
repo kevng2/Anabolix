47407d6bbbca450f58b77e07d8f95a1e44395796
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-10-01 09:32:30 -0400

    added a Kevin Button

--- app/src/main/java/com/android/gang/anabolix/MainActivity.java
+++ app/src/main/java/com/android/gang/anabolix/MainActivity.java
@@ -3,12 +3,24 @@ package com.android.gang.anabolix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mKevinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mKevinButton = findViewById(R.id.kevin_button);
+        mKevinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello, this is Kevin!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
