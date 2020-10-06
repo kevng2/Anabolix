fcebd529713289693f830c7ec3fca7db88aeb706
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-10-05 22:00:55 -0400

    added an activity for pedometer and basic layout

---
+++ app/src/main/java/com/android/gang/anabolix/PedometerActivity.java
@@ -0,0 +1,14 @@
+package com.android.gang.anabolix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PedometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedometer);
+    }
+}
