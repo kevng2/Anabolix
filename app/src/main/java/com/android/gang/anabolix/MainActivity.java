391cee6471748e4d15d2e180a6cc7d89b35b266c
Samuel Childers <samuel14920@gmail.com>
2020-10-08 02:55:34 -0400

    nav bar

--- app/src/main/java/com/android/gang/anabolix/MainActivity.java
+++ app/src/main/java/com/android/gang/anabolix/MainActivity.java
@@ -1,23 +1,40 @@
 package com.android.gang.anabolix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mKevinButton;
     private Button mSamButton;
     private Button mLewisButton;
     private Button mCarterButton;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navMethod);
+        bottomNavigationView.setItemTextColor(ColorStateList.valueOf(Color.RED));
+        bottomNavigationView.setItemIconTintList(ColorStateList.valueOf(Color.RED));
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+
+
+
 
         mKevinButton = findViewById(R.id.kevin_button);
         mKevinButton.setOnClickListener(new View.OnClickListener() {
@@ -52,5 +69,40 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "toooooast", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
+
     }
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            fragment = new HomeFragment();
+                            break;
+                        case R.id.exercise:
+                            fragment = new ExerciseFragment();
+                            break;
+                        case R.id.stats:
+                            fragment = new StatsFragment();
+                            break;
+                        case R.id.timer:
+                            fragment = new TimerFragment();
+                            break;
+                        case R.id.weather:
+                            fragment = new WeatherFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                    return true;
+                }
+            };
 }
