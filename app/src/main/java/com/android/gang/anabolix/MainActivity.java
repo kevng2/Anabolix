cfa0325ca9ba8f8ac2e719cf809d8d02c662441d
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-10-09 00:19:15 -0400

    added dependencies

--- app/src/main/java/com/android/gang/anabolix/MainActivity.java
+++ app/src/main/java/com/android/gang/anabolix/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         bottomNavigationView = findViewById(R.id.bottomNav);
         bottomNavigationView.setOnNavigationItemSelectedListener(navMethod);
         bottomNavigationView.setItemTextColor(ColorStateList.valueOf(Color.RED));
