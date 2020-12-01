f73967ebabbf09fd18096f5ee0beb1d09cd55b8c
Russell Lewis <Lewis.Russell.J@gmail.com>
2020-12-01 15:02:34 -0500

    Update LoginActivity.java
    Added catch for back button

--- app/src/main/java/com/android/gang/anabolix/LoginActivity.java
+++ app/src/main/java/com/android/gang/anabolix/LoginActivity.java
@@ -100,7 +100,12 @@ public class LoginActivity extends AppCompatActivity {
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button.
+                if (response == null){
+                    logged_in = false;
+                    signIn();
+                }
                 logged_in = false;
+                signIn();
             }
         }
     }
