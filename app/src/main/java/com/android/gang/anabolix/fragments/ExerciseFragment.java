5e7a07b272c78424f97839f9a82278514431f9c5
Tiffany Medina <t.medina1023@gmail.com>
2020-12-04 14:30:09 -0500

    Created ExerciseActivity

--- app/src/main/java/com/android/gang/anabolix/fragments/ExerciseFragment.java
+++ app/src/main/java/com/android/gang/anabolix/fragments/ExerciseFragment.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
